620971d83f34b933491850ddff0390f593ea24f1
KDhieb <khaliddhieb@gmail.com>
2021-07-05 14:56:52 -0400

    update

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const rateLimit = require("express-rate-limit");
 const router = express.Router();
 
 // middleware
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 
 // ! ENABLE FOR DEPLOYMENT !!!!!!
 app.set("trust proxy", 1); //
@@ -22,32 +22,32 @@ app.use(express.json());
 
 // Static routes
 // Serve React build files in production
-if (process.env.NODE_ENV === "production") {
-  const path = require("path");
-  // Serve the frontend's index.html file at the root route
-  router.get("/", (req, res) => {
-    res.cookie("XSRF-TOKEN", req.csrfToken());
-    res.sendFile(
-      path.resolve(__dirname, "../../frontend", "build", "index.html")
-    );
-  });
-  // Serve the static assets in the frontend's build folder
-  router.use(express.static(path.resolve("../build")));
-  // Serve the frontend's index.html file at all other routes NOT starting with /api
-  router.get(/^(?!\/?api).*/, (req, res) => {
-    res.cookie("XSRF-TOKEN", req.csrfToken());
-    res.sendFile(
-      path.resolve(__dirname, "../../frontend", "build", "index.html")
-    );
-  });
-}
+// if (process.env.NODE_ENV === "production") {
+//   const path = require("path");
+//   // Serve the frontend's index.html file at the root route
+//   router.get("/", (req, res) => {
+//     res.cookie("XSRF-TOKEN", req.csrfToken());
+//     res.sendFile(
+//       path.resolve(__dirname, "../../frontend", "build", "index.html")
+//     );
+//   });
+// Serve the static assets in the frontend's build folder
+//   router.use(express.static(path.resolve("../build")));
+//   // Serve the frontend's index.html file at all other routes NOT starting with /api
+//   router.get(/^(?!\/?api).*/, (req, res) => {
+//     res.cookie("XSRF-TOKEN", req.csrfToken());
+//     res.sendFile(
+//       path.resolve(__dirname, "../../frontend", "build", "index.html")
+//     );
+//   });
+// }
 // Add a XSRF-TOKEN cookie in development
-if (process.env.NODE_ENV !== "production") {
-  router.get("/api/csrf/restore", (req, res) => {
-    res.cookie("XSRF-TOKEN", req.csrfToken());
-    res.status(201).json({});
-  });
-}
+// if (process.env.NODE_ENV !== "production") {
+//   router.get("/api/csrf/restore", (req, res) => {
+//     res.cookie("XSRF-TOKEN", req.csrfToken());
+//     res.status(201).json({});
+//   });
+// }
 
 //ROUTES
 
